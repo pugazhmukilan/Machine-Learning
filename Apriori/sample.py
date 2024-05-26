8e0df59b9b75f4d0ed8566fddd653dfe8715adcd
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2024-05-26 18:08:00 +0530

    elcat model

---
+++ Apriori/sample.py
@@ -0,0 +1,6 @@
+RelationRecord(items=frozenset({'chicken', 'extra dark chocolate'}),
+               support=0.0027996267164378083, 
+               ordered_statistics=[OrderedStatistic(items_base=frozenset({'extra dark chocolate'}), 
+                                                    items_add=frozenset({'chicken'}), 
+                                                    confidence=0.23333333333333334, 
+                                                    lift=3.8894074074074076)])
